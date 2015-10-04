03239c57e363e3e0030018d3d6f593a8d6af3d8f
Jason Stangroome <jason@section.io>
2015-10-04 16:25:41 +1100

    Implement `ping` via a read loop

--- main.go
+++ main.go
@@ -1,12 +1,32 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"net/http"
-	//"io"
+	"io"
 	"io/ioutil"
 	"log"
 	"net"
+	"net/http"
+	"strings"
+	"time"
+)
+
+type VarnishCliResponseStatus int
+
+const (
+	CLIS_SYNTAX    VarnishCliResponseStatus = 100
+	CLIS_UNKNOWN                            = 101
+	CLIS_UNIMPL                             = 102
+	CLIS_TOOFEW                             = 104
+	CLIS_TOOMANY                            = 105
+	CLIS_PARAM                              = 106
+	CLIS_AUTH                               = 107
+	CLIS_OK                                 = 200
+	CLIS_TRUNCATED                          = 201
+	CLIS_CANT                               = 300
+	CLIS_COMMS                              = 400
+	CLIS_CLOSE                              = 500
 )
 
 var listenPort = 6083 // normally 6082
@@ -27,65 +47,86 @@ func main() {
 	for {
 		connection, err := listener.Accept()
 		if err != nil {
-			log.Fatal(err)
+			log.Fatal(err) // panic instead?
 		}
 		go handleConnection(connection)
 	}
-	err = listener.Close()
+	err = listener.Close() // defer instead
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func writeVarnishCliResponse(connection net.Conn, status int, body string) error {
-	responseLength := len(body)
+func writeVarnishCliResponse(writer io.Writer, status VarnishCliResponseStatus, body string) {
+	responseLength := len(body) // NOTE len() returns byte count, not character count
 	statusLine := fmt.Sprintf("%3d %8d\n", status, responseLength)
 	buffer := []byte(statusLine + body + "\n")
 
-	bytesWritten, err := connection.Write(buffer)
+	bytesWritten, err := writer.Write(buffer)
 	if err != nil {
-		log.Print(err)
+		log.Panic(err)
 	} else {
 		log.Printf("Wrote %d bytes", bytesWritten)
 	}
-
-	return err
 }
 
-func writeVarnishCliAuthenticationChallenge(connection net.Conn) error {
+func writeVarnishCliAuthenticationChallenge(writer io.Writer) {
 	randomChallenge := "abcdefghijabcdefghijabcdefghijkl" // TODO randomise
-	err := writeVarnishCliResponse(connection, 107, randomChallenge)
-	return err
+	writeVarnishCliResponse(writer, CLIS_AUTH, randomChallenge)
 }
 
-func readVarnishCliAuthenticationAttempt(connection net.Conn) error {
-	buffer := make([]byte, 512)
-	bytesRead, err := connection.Read(buffer)
-	if err != nil {
-		log.Print(err)
-		return err
+func handleVarnishCliAuthenticationAttempt(args string, writer io.Writer) {
+
+	log.Printf("Auth attempt '%s'", args)
+
+	// TODO verify secret, if auth failed, resend challenge
+
+	writeVarnishCliResponse(writer, CLIS_OK, "Welcome")
+
+}
+
+func handleVarnishCliPingRequest(writer io.Writer) {
+	response := fmt.Sprintf("PONG %d 1.0", time.Now().Unix())
+	writeVarnishCliResponse(writer, CLIS_OK, response)
+}
+
+func handleRequest(requestLine string, writer io.Writer) {
+	requestLine = strings.TrimLeft(requestLine, " ")
+	commandAndArgs := strings.SplitN(requestLine, " ", 2)
+	command := commandAndArgs[0]
+	if command != strings.ToLower(command) {
+		writeVarnishCliResponse(writer, CLIS_UNKNOWN, "all commands are in lower-case.")
+		return
 	}
-	log.Printf("read %d bytes", bytesRead)
 
-	// TODO verify secret
+	switch command {
+	case "auth":
+		handleVarnishCliAuthenticationAttempt(commandAndArgs[1], writer)
+		return
+	case "ping":
+		handleVarnishCliPingRequest(writer)
+		return
+	}
 
-	return nil
+	log.Printf("Unrecognised command '%s'.", command)
+	writeVarnishCliResponse(writer, CLIS_UNIMPL, "Unimplemented")
 }
 
 func handleConnection(connection net.Conn) {
 	defer connection.Close()
+	scanner := bufio.NewScanner(connection)
 
-	err := writeVarnishCliAuthenticationChallenge(connection)
-	if err != nil {
-		log.Print(err)
+	writeVarnishCliAuthenticationChallenge(connection)
+
+	for {
+		if scanner.Scan() {
+			handleRequest(scanner.Text(), connection)
+		} else {
+			break
+		}
 	}
 
-	err = writeVarnishCliResponse(connection, 200, "Welcome")
-	if err != nil {
-		log.Print(err)
-	}
-
-	err = readVarnishCliAuthenticationAttempt(connection)
+	err := scanner.Err()
 	if err != nil {
 		log.Print(err)
 	}
