e02f284312b972c6455c934f54b278ef5e3ba1c9
Jason Stangroome <jason@section.io>
2015-10-21 21:00:59 +1100

    Add test for commit 509ede1 (left aligned length field in status line)

--- main_test.go
+++ main_test.go
@@ -1,6 +1,17 @@
 package main
 
 import "testing"
+import (
+	"bytes"
+	"strings"
+)
 
-func TestPass(t *testing.T) {
+func TestCliResponseStatusLineLengthFieldIsLeftAligned(t *testing.T) {
+	expected := "200 4       \n"
+	mockWriter := new(bytes.Buffer)
+	writeVarnishCliResponse(mockWriter, CLIS_OK, "four")
+	actual := mockWriter.String()
+	if !strings.HasPrefix(actual, expected) {
+		t.Errorf("Expected response to being with '%#v' but was '%#v'.", expected, actual)
+	}
 }
