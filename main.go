ba48595914cbfba8d48e90af5c15f197519b612a
Ivan Hamilton <ivan@section.io>
2015-11-25 12:01:31 +1100

    Handle param.show esi_syntax & cli_buffer request

--- main.go
+++ main.go
@@ -305,6 +305,34 @@ func handleVarnishCliBanRequest(args string, writer io.Writer) {
 		fmt.Sprintf("API responded with status %d.", response.StatusCode))
 }
 
+func handleVarnishCliParamShowRequest(arg string, writer io.Writer) {
+	switch arg {
+	case "esi_syntax":
+		writeVarnishCliResponse(writer, CLIS_OK, `esi_syntax                  2 [bitmap]
+                            Default is 0
+                            Bitmap controlling ESI parsing code:
+                              0x00000001 - Don't check if it looks like XML
+                              0x00000002 - Ignore non-esi elements
+                              0x00000004 - Emit parsing debug records
+                              0x00000008 - Force-split parser input
+                            (debugging)
+                            Use 0x notation and do the bitor in your head :-)
+`)
+
+	case `cli_buffer`:
+		writeVarnishCliResponse(writer, CLIS_OK, `cli_buffer                  32768 [bytes]
+                            Default is 8192
+                            Size of buffer for CLI input.
+                            You may need to increase this if you have big VCL
+                            files and use the vcl.inline CLI command.
+                            NB: Must be specified with -p to have effect.
+`)
+	default:
+		writeVarnishCliResponse(writer, CLIS_PARAM,
+			fmt.Sprintf("Unknown parameter \"%s\".", arg))
+	}
+}
+
 func handleRequest(requestLine string, session *VarnishCliSession) {
 	log.Printf("Received request %#v", requestLine)
 	requestLine = strings.TrimLeft(requestLine, " ")
@@ -340,6 +368,9 @@ func handleRequest(requestLine string, session *VarnishCliSession) {
 	}
 
 	switch command {
+	case "param.show":
+		handleVarnishCliParamShowRequest(commandAndArgs[1], session.Writer)
+		return
 	case "ban":
 		handleVarnishCliBanRequest(varnishQuoteArgs(commandAndArgs[1:]), session.Writer)
 		return
