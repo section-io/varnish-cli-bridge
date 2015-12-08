71f5ea154e946eb58d4b9bbe3f2a5f6a4fa01c79
Ivan Hamilton <ivan@section.io>
2015-12-08 17:20:36 +1100

    Pass vcl.inline/use to section.io

--- handle_vcl_use.go
+++ handle_vcl_use.go
@@ -3,8 +3,14 @@ package main
 import (
 	"fmt"
 	"io"
-	"os"
+	"log"
 )
+
+type jsonUpdateVclRequest struct {
+	Personality string `json:"personality"`
+	Message     string `json:"message"`
+	Content     string `json:"content"`
+}
 
 func handleVarnishCliVclUse(configname string, writer io.Writer) {
 	if configname != inlineConfigName {
@@ -12,12 +18,21 @@ func handleVarnishCliVclUse(configname string, writer io.Writer) {
 		return
 	}
 
-	os.Remove("./vcl.vcl")
-	f, _ := os.Create("./vcl.vcl")
-	f.WriteString(inlineQuotedVCLstring)
-	f.Close()
-
 	//Post it to the API
+	postValues := jsonUpdateVclRequest{
+		Personality: "MagentoTurpentine",
+		Message:     "Update from varnish-cli-bridge",
+		Content:     inlineQuotedVCLstring,
+	}
+
+	response, err := jsonPost(writer, sectionioApiEndpoint+"configuration", "configuration update", postValues)
+
+	if err != nil {
+		//CLI Response already written on non-200 or other error
+		return
+	}
+
+	log.Printf("Update submitted. Response message: %s", response["message"])
 
 	//Varnishd actually returns a 200 & zero byte reponse (a problem to match since we add a trailing /n in writeVarnishCliResponse)
 	writeVarnishCliResponse(writer, CLIS_OK, ``)
